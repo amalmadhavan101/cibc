58d028b4f31bec37cbd76cce2c5f9bde5dba7951
amalmadhavan101 <133011892+amalmadhavan101@users.noreply.github.com>
2024-01-25 14:38:48 -0500

    Update sql_script1.sql

--- sql_script1.sql
+++ sql_script1.sql
@@ -1,4 +1,5 @@
 use amal124;
 select * from users2;
+limit 10;
 -- insert into users2 values(3,'amal','abc@gmail.com'),(4,'asssdf','adf@hn.com'),(5,'gk','gk@gmail.com'),(6,'gkk','gkkk@gmail.com');
 -- select * from users2;
